58e0d312f777c6eb59ab160ba8b7a0d4c808b327
KLCela45 <klea0312cela@gmail.com>
2026-05-18 21:39:17 +0200

    Week 3 done

--- Entity/ProductVolumes.cs
+++ Entity/ProductVolumes.cs
@@ -1,11 +1,14 @@
+using System.Text.Json.Serialization;
+
 namespace Scente.API.Entity;
 
 public class ProductVolume
 {
     public int Id { get; set; }
     public int ProductId { get; set; }
-    public string Size { get; set; } = string.Empty;   // "30ml", "50ml", "100ml"
+    public string Size { get; set; } = string.Empty;
     public decimal Price { get; set; }
 
+    [JsonIgnore]
     public Product Product { get; set; } = null!;
 }
