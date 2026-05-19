016e88bcb79383eca7caeabf29374d28c3db423e
j d <jdurmishi06@gmail.com>
2026-05-19 19:59:26 +0200

    Finish JWT authentication and validation system

--- DTOs/RegisterDto.cs
+++ DTOs/RegisterDto.cs
@@ -1,9 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Scente.API.DTOs;
 
 public class RegisterDto
 {
+    [Required]
+    [MinLength(2)]
     public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(2)]
     public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(6)]
     public string Password { get; set; } = string.Empty;
 }
