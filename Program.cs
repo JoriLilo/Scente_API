0da4e0cc78ef1351da798907494f144404d9f47a
Jorida <147238922+JoriLilo@users.noreply.github.com>
2026-06-04 00:17:54 +0200

    FINAL

--- Program.cs
+++ Program.cs
@@ -68,12 +68,13 @@ builder.Services.AddScoped<IEmailService, EmailService>();
 
 builder.Services.AddCors(options =>
     options.AddPolicy("FrontendPolicy", policy =>
-        policy.WithOrigins(
-            "http://localhost:3000",
-            "http://127.0.0.1:5500",
-            "http://127.0.0.1:5501",
-            "http://localhost:5501"
-        )
+       policy.WithOrigins(
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501",
+    "http://localhost:5500",   
+    "http://localhost:5501"
+)
         .AllowAnyHeader()
         .AllowAnyMethod()
 ));
